6cc6570c23f8fda1afbca7f7d71b3fd07bd387e3
adarraji <a.darraji@outlook.com>
2023-04-01 12:12:03 -0400

    installed fontawesome, added icon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,12 @@
+import { faStar } from "@fortawesome/free-regular-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 export const Header = () => {
     return (
         <div>
+            <FontAwesomeIcon icon={faStar} />
             <span>Joseph Smith</span>
             <button>New Order</button>
-        </div>
+        </div >
     )
 }
