aff00d01e259914693cd9a1e1cf6f5db86627818
adarraji <a.darraji@outlook.com>
2023-04-02 18:33:56 -0400

    organized file

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 import { Header } from './components/header/Header';
 import styles from "./app.module.scss";
 import "./fontawesome/css/all.css"
+import { Tabs } from './components/tabs/Tabs';
 
 function App() {
   return (
     <div className={styles.app}>
       <div className={styles.container}>
         <Header />
+        <Tabs />
       </div>
     </div>
   );
