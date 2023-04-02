109b14becffbc24721e594502a6ba2ad7f637035
adarraji <a.darraji@outlook.com>
2023-04-02 16:03:49 -0400

    updated activity comp. and styles

--- src/components/commActivity/CommActivity.tsx
+++ src/components/commActivity/CommActivity.tsx
@@ -6,12 +6,20 @@ export const CommActivity = () => {
             <div className={styles.header}>
                 <span>90-Day COMMUNICATION ACTIVITY</span>
             </div>
-            {/* <div className={styles.info}>
-                <div>
+            <div className={styles.info}>
+                <div className={styles.activityCotainer}>
                     <div className={styles.number}>6</div>
                     <div className={styles.activity}>SMS</div>
                 </div>
-            </div> */}
+                <div className={styles.activityCotainer}>
+                    <div className={styles.number}>4</div>
+                    <div className={styles.activity}>EMAIL</div>
+                </div>
+                <div className={styles.activityCotainer}>
+                    <div className={styles.number}>1</div>
+                    <div className={styles.activity}>ORDERS</div>
+                </div>
+            </div>
 
         </div>
     )
