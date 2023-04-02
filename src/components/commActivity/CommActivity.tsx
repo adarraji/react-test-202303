532a7b5e7020b5060be13cc7a8cd6ee85445518e
adarraji <a.darraji@outlook.com>
2023-04-02 15:35:15 -0400

    updated  activity comp. and styles

--- src/components/commActivity/CommActivity.tsx
+++ src/components/commActivity/CommActivity.tsx
@@ -2,6 +2,17 @@ import styles from "./commActivity.module.scss";
 
 export const CommActivity = () => {
     return (
-        <div>CommActivity</div>
+        <div className={styles.commActivity}>
+            <div className={styles.header}>
+                <span>90-Day COMMUNICATION ACTIVITY</span>
+            </div>
+            {/* <div className={styles.info}>
+                <div>
+                    <div className={styles.number}>6</div>
+                    <div className={styles.activity}>SMS</div>
+                </div>
+            </div> */}
+
+        </div>
     )
 }
