c2f83b5ad227fdeb7f2a09e223e80181942f2f1f
adarraji <a.darraji@outlook.com>
2023-04-02 14:11:09 -0400

    updated styles

--- src/components/userInfo/UserInfo.tsx
+++ src/components/userInfo/UserInfo.tsx
@@ -4,10 +4,27 @@ import styles from "./userInfo.module.scss";
 export const UserInfo = () => {
   return (
     <div className={styles.userInfo}>
-      <div className={styles.userId}>
-        <i className="fal fa-light fa-user"/>       
+      <div>
+        <i className="fal fa-light fa-user" />
         <span>#12345678</span>
       </div>
+      <div>
+        <i className="fal fa-light fa-user" />
+        <span>248-555-3000</span>
+      </div>
+      <div>
+        <i className="fal fa-light fa-user" />
+        <span>248-555-2000</span>
+      </div>
+      <div>
+        <i className="fal fa-light fa-user" />
+        <span>248-555-1000</span>
+      </div>
+      <div>
+        <i className="fal fa-light fa-user" />
+        <span>joe.smith@testemail.com</span>
+      </div>
+
     </div>
   )
 }
