c26b3799d8fc44f9d146690c23a3b50c2ee4d9f9
adarraji <a.darraji@outlook.com>
2023-04-02 12:36:33 -0400

    added user info, updated styles

--- src/components/userInfo/UserInfo.tsx
+++ src/components/userInfo/UserInfo.tsx
@@ -1,8 +1,15 @@
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./userInfo.module.scss";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
 
 export const UserInfo = () => {
   return (
-    <div>UserDetails</div>
+    <div className={styles.userInfo}>
+      <div className={styles.userId}>
+        <FontAwesomeIcon icon={faUser} className={styles.icon} />
+        <div className={styles.id}>#12345678</div>
+      </div>
+    </div>
   )
 }
