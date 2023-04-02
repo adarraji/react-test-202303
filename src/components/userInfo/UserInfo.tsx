518944f638cfd20a143b6482202747ca999927fa
adarraji <a.darraji@outlook.com>
2023-04-02 14:50:26 -0400

    updated styles

--- src/components/userInfo/UserInfo.tsx
+++ src/components/userInfo/UserInfo.tsx
@@ -5,23 +5,23 @@ export const UserInfo = () => {
   return (
     <div className={styles.userInfo}>
       <div>
-        <i className="fal fa-light fa-user" />
+        <i className="fal fa-user" />
         <span>#12345678</span>
       </div>
       <div>
-        <i className="fal fa-light fa-user" />
+        <i className="fal fa-mobile" />
         <span>248-555-3000</span>
       </div>
       <div>
-        <i className="fal fa-light fa-user" />
+        <i className="fal fa-building" />
         <span>248-555-2000</span>
       </div>
       <div>
-        <i className="fal fa-light fa-user" />
+        <i className="fal fa-house" />
         <span>248-555-1000</span>
       </div>
       <div>
-        <i className="fal fa-light fa-user" />
+        <i className="fal fa-at" />
         <span>joe.smith@testemail.com</span>
       </div>
 
