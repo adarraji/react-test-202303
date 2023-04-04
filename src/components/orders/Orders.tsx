1de8b95fe2bf3d41fc29eed0cdb02de6cb9b126b
adarraji <a.darraji@outlook.com>
2023-04-04 18:13:35 -0400

    using axios to fetch data and pass it as props

--- src/components/orders/Orders.tsx
+++ src/components/orders/Orders.tsx
@@ -1,38 +1,36 @@
+import axios from "axios";
 import { SubTabs } from "./SubTabs";
 import { Table } from "./Table";
 import { Tabs } from "./Tabs";
 import styles from "./orders.module.scss";
+import { useEffect, useState } from "react";
 
-export const Orders = () => {
+export const Orders = () => {    
 
-    const data = [
-        {
-            "id": 1,
-            "order_id": 12345546,
-            "sent_dt": "2018-04-14",
-            "sent_tm": "16:19:00",
-            "subject": {
-                "title": "Thank You Bonus",
-                "email": "joe.smith@testemail.com"
-            },
-            "type": "Promotion Email"
-        },
-        {
-            "id": 2,
-            "order_id": 12345546,
-            "sent_dt": "2018-04-14",
-            "sent_tm": "16:19:00",
-            "subject": {
-                "title": "Receipt #12345546",
-                "email": "joe.smith@testemail.com"
-            },
-            "type": "Order Receipt Email"
+    const [orders, setOrders] = useState({});
+
+    useEffect(() => {
+        getOrders()
+    }, [])
+
+    const getOrders = async () => {
+        try {
+            const response = await axios("https://evoteam-verasoft.github.io/data/orders.json");
+            const orders = await response.data;
+            setOrders(orders)
+            console.log(orders)
+
+        } catch (err) {
+            console.log(err);
         }
-    ]
+
+    }
+
+
 
     return (
         <div className={styles.orders}>
-            <Tabs orders={data} />
+            <Tabs orders={orders} />
             <SubTabs />
             <Table />
         </div>
