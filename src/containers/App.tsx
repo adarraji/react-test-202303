ef36b36ede35c7fadb9e84c54c3f1b1f615f6772
adarraji <a.darraji@outlook.com>
2023-04-03 22:16:54 -0400

    organizing files

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,8 +1,8 @@
 import './App.css';
-import { Header } from './components/header/Header';
+import { Header } from '../components/header/Header';
 import styles from "./app.module.scss";
-import "./fontawesome/css/all.css"
-import { Orders } from './components/orders/Orders';
+import "../fontawesome/css/all.css"
+import { Orders } from '../components/orders/Orders';
 
 function App() {
   return (
